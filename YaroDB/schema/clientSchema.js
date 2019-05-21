9b2304fcd169c7fe5a19a7d5daeec0384a44e84f
jeanfern5 <jeanfern555@gmail.com>
2019-05-21 12:25:02 -0500

    Cleaned up graphql client schema.

--- YaroDB/schema/clientSchema.js
+++ YaroDB/schema/clientSchema.js
@@ -2,35 +2,30 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-    type Client.CLAIMS {
+    type CLAIMS {
         claim_id: ID!,
         yaro_user_id: ID!,
         client_id: ID!,
         claim_type_id: ID!,
         claim_amount: INT!
     }
-    type Client.CLAIM_TYPE {
+    type CLAIM_TYPE {
         client_type_id: ID!,
         procedure: String!,
         description: String!,
     }
-    input Client.CLAIMS_INPUT{
+    input CLAIMS_INPUT{
         claim_amount: INT!  
     }
-    type Client.CLAIM_TYPES_INPUT {
+    type CLAIM_TYPES_INPUT {
         procedure: String!,
         description: String!,
     }
     type RootQuery {
-        listClaims: [Client.CLAIMS!]!
-        getClaims(claim_id: ID!): Client.CLAIMS!
-    }
-    type RootMutation {
-        createClaim(clientClaimsInput: Client.CLAIMS_INPUT!): YARO_USER
-        createClaimType(clientClaimsTypeInput: Client.CLAIM_TYPES_INPUT!): YARO_CLIENT
+        listClaims: [CLAIMS!]!
+        getClaims(claimId: ID!): CLAIMS!
     }
     schema {
         query: RootQuery
-        mutation: RootMutation
     }
 `)
